69aa0ceb7ee111178c2cffc6476469c02feb070d
Sina Siadat <siadat@gmail.com>
2024-01-06 13:35:49 +0000

    a function to convert construct structs to s-exprs

--- sstable/sstable_data.py
+++ sstable/sstable_data.py
@@ -240,3 +240,93 @@ partition = construct.Struct(
 )
 
 data_format = construct.Struct("partitions" / sstable.greedy_range.GreedyRangeWithExceptionHandling(partition))
+
+import textwrap
+import inspect
+def convert_construct(x, depth=0):
+    prefix = "    " * depth
+    prefix2 = "    " * (depth+1)
+    name = x.__class__.__name__
+    if name == "Struct":
+        return "\n".join([
+            prefix + "(" + name,
+            "\n".join([convert_construct(subcon, depth+1) for subcon in x.subcons]),
+            prefix + ")",
+        ])
+    elif name == "Renamed":
+        name = "Field"
+        return "\n".join([
+            prefix + "(" + name + " " + repr(str(x.name)),
+            convert_construct(x.subcon,depth+1),
+            prefix + ")",
+        ])
+    elif name == "FormatField":
+        return prefix + "(" + name + " " + repr(str(x.fmtstr)) + ")"
+    elif name == "Bytes":
+        return prefix + "(" + name + " " + str(x.length) + ")"
+    elif name == "Array":
+        return "\n".join([
+            prefix + "(" + name,
+            prefix2 + f"{x.count}",
+            convert_construct(x.subcon,depth+1),
+            prefix + ")",
+        ])
+    elif name == "Pass":
+        return prefix + "(" + name + ")"
+    elif name == "Tell":
+        return prefix + "(" + name + ")"
+    elif name == "EnabledColumns":
+        return prefix + "(" + name + ")"
+    elif name == "VarInt":
+        return prefix + "(" + name + ")"
+    elif name == "WithContext":
+        return "\n".join([
+            prefix + "(" + name,
+            prefix2 + f"{x.kw_ctx_funcs}",
+            convert_construct(x.subcon,depth+1),
+            prefix + ")",
+        ])
+    elif name == "Hex":
+        return "\n".join([
+            prefix + "(" + name,
+            convert_construct(x.subcon,depth+1),
+            prefix + ")",
+        ])
+    elif name == "RepeatUntil":
+        return "\n".join([
+            prefix + "(" + name,
+            prefix2 + f"{x.predicate}",
+            convert_construct(x.subcon,depth+1),
+            prefix + ")",
+        ])
+    elif name == "Switch":
+        return "\n".join([
+            prefix + "(" + name,
+            prefix2 + f"{x.keyfunc}",
+            prefix + ")",
+        ])
+    elif name == "DynamicSwitch":
+        return "\n".join([
+            prefix + "(" + name,
+            prefix2 + f"{x.key_predicate}",
+            prefix2 + f"{x.value_func}",
+            prefix + ")",
+        ])
+    elif name == "GreedyRangeWithExceptionHandling":
+        return "\n".join([
+            prefix + "(" + name,
+            convert_construct(x.subcon,depth+1),
+            prefix + ")",
+        ])
+    elif name == "IfThenElse":
+        return "\n".join([
+            prefix + "(" + name,
+            prefix2 + f"{x.condfunc}",
+            convert_construct(x.thensubcon,depth+1),
+            convert_construct(x.elsesubcon,depth+1),
+            prefix + ")",
+        ])
+    else:
+        raise Exception(f"Unknown construct: {name}")
+
+print(convert_construct(data_format))
