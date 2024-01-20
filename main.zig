0482544c9ec51cdd9a84ff3c5337c62dd6b5d7d7
Sina Siadat <siadat@gmail.com>
2024-01-20 22:20:39 +0000

    remove unnecessary comments

--- main.zig
+++ main.zig
@@ -9,19 +9,15 @@ pub fn open() !std.fs.File {
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
-    // Open the file for reading
     const file = try open();
     defer file.close();
 
-    // Create a buffer for a single byte
     var buffer: [1]u8 = undefined;
 
-    // Read from the file, one byte at a time
     while (true) {
         const bytesRead = try file.reader().read(buffer[0..]);
         if (bytesRead == 0) break; // End of file reached
 
-        // Process or print the read byte
         try stdout.print("0x{x:0>2} ", .{buffer[0]});
         try stdout.print("{d: >3} ", .{buffer[0]});
         try stdout.print("'", .{});
