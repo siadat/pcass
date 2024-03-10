cb4aa0d3674c867c6eda9067e18e9260d5e8bef6
Sina Siadat <siadat@gmail.com>
2024-03-10 13:01:22 +0000

    move hexdump.bash to my dotfiles

--- hexdump.bash
+++ hexdump.bash
@@ -1,2 +1,4 @@
+# THIS IS MOVED TO bash_utils IN MY DOTFILES REPO
+
 # print one byte (and its ascii representation) per line
-hexdump -v -e '1/1 "0x%02x "' -e '1/1 "%_p\n"' "$@"
+# hexdump -v -e '1/1 "0x%02x "' -e '1/1 "%_p\n"' "$@"
