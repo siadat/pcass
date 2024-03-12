4b7085044dbf0ac14b8f8184958bb6bee83ddd34
Sina Siadat <siadat@gmail.com>
2024-03-12 01:08:44 +0000

    making each field in fromBytes very similar to each other

--- server.zig
+++ server.zig
@@ -66,11 +66,11 @@ const FrameHeader = packed struct {
     ) void {
         switch (endian) {
             .little => {
-                self.version = std.mem.bytesAsValue(@TypeOf(self.version), buf[0..1]).*;
-                self.flags = std.mem.bytesAsValue(@TypeOf(self.flags), buf[1..2]).*;
-                self.stream = @byteSwap(std.mem.bytesAsValue(i16, buf[2..4]).*);
-                self.opcode = std.mem.bytesAsValue(@TypeOf(self.flags), buf[4..5]).*;
-                self.length = @byteSwap(std.mem.bytesAsValue(u32, buf[5..9]).*);
+                self.version = @byteSwap(std.mem.bytesAsValue(@TypeOf(self.version), buf[0..1]).*);
+                self.flags = @byteSwap(std.mem.bytesAsValue(@TypeOf(self.flags), buf[1..2]).*);
+                self.stream = @byteSwap(std.mem.bytesAsValue(@TypeOf(self.stream), buf[2..4]).*);
+                self.opcode = @byteSwap(std.mem.bytesAsValue(@TypeOf(self.flags), buf[4..5]).*);
+                self.length = @byteSwap(std.mem.bytesAsValue(@TypeOf(self.length), buf[5..9]).*);
             },
             .big => self.* = std.mem.bytesAsValue(FrameHeader, &buf).*,
         }
