c437c265957328619e7469959e20ae51855fe09f
Sina Siadat <siadat@gmail.com>
2024-04-01 20:53:33 +0000

    ah, forget it, I don't need a ClientConnection struct yet

--- server.zig
+++ server.zig
@@ -221,26 +221,6 @@ pub fn copyReverse(comptime T: type, dest: []T, source: []const T) void {
     }
 }
 
-const ClientConnection = struct {
-    allocator: std.mem.Allocator,
-    client: *net.Server.Connection,
-    logger: Logger,
-
-    fn init(allocator: std.mem.Allocator, client: *net.Server.Connection, logger: Logger) ClientConnection {
-        logger.debug("client connected: {any}", .{client.address});
-        return ClientConnection{
-            .logger = logger,
-            .allocator = allocator,
-            .client = client,
-        };
-    }
-
-    fn deinit(self: *ClientConnection) void {
-        self.logger.debug("client disconnected: {any}", .{self.client.address});
-        self.client.stream.close();
-    }
-};
-
 const CqlServer = struct {
     net_server: *net.Server,
     state_machine: StateMachine,
@@ -300,10 +280,9 @@ const CqlServer = struct {
     //
 
     fn handleClient(self: *@This(), client: *net.Server.Connection) !void {
-        self.logger.debug("debug s message", .{});
-
-        var client_conn = ClientConnection.init(self.allocator, client, self.logger);
-        defer client_conn.deinit();
+        self.logger.debug("client connected: {any}", .{client.address});
+        defer self.logger.debug("client disconnected: {any}", .{client.address});
+        defer client.stream.close();
 
         var total_bytes_count: usize = 0;
         defer self.logger.debug("total bytes: {d}", .{total_bytes_count});
